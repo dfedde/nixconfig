3582e3d1f3b4bc0f17062a6f8d2d58025f2003eb
dyan Fedde <dyan@fedde.us>
2025-04-26 19:01:29 -0600

    remove extra declaration

--- users/dyan.nix
+++ users/dyan.nix
@@ -13,13 +13,13 @@
     # The state version is required and should stay at the version you
     # originally installed.
     home.stateVersion = "24.11";
+
     programs.git = {
     	enable = true;
 	userName = "dyan Fedde";
 	userEmail = "dyan@fedde.us";
     };
 
-    shell = pkgs.zsh;
     programs.zsh = {
     	enable = true;
 	shellAliases = {
