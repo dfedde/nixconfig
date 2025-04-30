3b9b3df98816405a3343ff35be9e31511c12090b
dyan Fedde <dyan@fedde.us>
2025-04-30 17:08:43 -0600

    nixvim

--- users/nixvim.nix
+++ users/nixvim.nix
@@ -3,7 +3,7 @@ let
   nixvim = import (builtins.fetchGit {
     url = "https://github.com/nix-community/nixvim";
     # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-    # ref = "nixos-24.11";
+    ref = "nixos-24.11";
   });
 in
 {
