514ca37fb92515553ecb2621aefe97b33212c1af
dyan Fedde <dyan@fedde.us>
2025-05-02 13:50:28 -0600

    make ssh/config loop a bit better

--- users/dyan.nix
+++ users/dyan.nix
@@ -2,55 +2,55 @@
 { config, pkgs, ... }: {
 
 
-  programs = {
-  zsh.enable = true;
-  _1password.enable = true;
-  _1password-gui.enable = true;
-  };
-
-  users.users.dyan = {
-    isNormalUser = true;
-    description = "it's me";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  home-manager.users.dyan = { pkgs, ... }: {
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home.stateVersion = "24.11";
-
-    programs.git = {
-    	enable = true;
-	userName = "dyan Fedde";
-	userEmail = "dyan@fedde.us";
-    };
-
-    programs.zsh = {
-    	enable = true;
-	shellAliases = {
+	programs = {
+		zsh.enable = true;
+		_1password.enable = true;
+		_1password-gui.enable = true;
 	};
 
-    };
-
-    programs.neovim = {
-    	enable = true;
-	defaultEditor = true;
-	vimAlias = true;
-	extraPackages = [ pkgs.gcc ]; # this is a hack so lazy can do some compiling
-    };
-
-    programs.ssh = {
-        enable = true;
-	extraConfig = ''
-		Host *
-			IdentityAgent ~/.1password/agent.sock
-	'';
+	users.users.dyan = {
+		isNormalUser = true;
+		description = "it's me";
+		extraGroups = [ "networkmanager" "wheel" ];
+		shell = pkgs.zsh;
 	};
 
-    xdg.configFile.nvim = {
-    	source = ./nvim;
-	recursive = true;
-    };
-  };
-}
+	home-manager.users.dyan = { pkgs, ... }: {
+# The state version is required and should stay at the version you
+# originally installed.
+		home.stateVersion = "24.11";
+
+		programs.git = {
+			enable = true;
+			userName = "dyan Fedde";
+			userEmail = "dyan@fedde.us";
+		};
+
+		programs.zsh = {
+			enable = true;
+			shellAliases = {
+			};
+
+		};
+
+		programs.neovim = {
+			enable = true;
+			defaultEditor = true;
+			vimAlias = true;
+			extraPackages = [ pkgs.gcc ]; # this is a hack so lazy can do some compiling
+		};
+
+		programs.ssh = {
+			enable = true;
+			extraConfig = ''
+				Host *
+					IdentityAgent ~/.1password/agent.sock
+				'';
+		};
+
+		xdg.configFile.nvim = {
+			source = ./nvim;
+			recursive = true;
+		};
+	};
+											 }
