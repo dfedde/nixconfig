0ad085491e0118dd2d00fe231be7515cbb686b34
dyan Fedde <dyan@fedde.us>
2025-05-02 18:50:44 -0600

    use system podman

--- users/dyan.nix
+++ users/dyan.nix
@@ -17,6 +17,27 @@
     shell = pkgs.zsh;
   };
 
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    docker-compose # start group of containers for dev
+    # podman-compose # start group of containers for dev
+  ];
+
   home-manager.users.dyan =
     { pkgs, ... }:
     {
@@ -29,10 +50,6 @@
         podman
         podman-compose
       ];
-
-      services = {
-        podman.enable = true;
-      };
 
       programs = {
         alacritty = {
@@ -50,7 +67,6 @@
           syntaxHighlighting.enable = true;
           shellAliases = {
             gst = "git status";
-            docker = "podman";
           };
         };
 
