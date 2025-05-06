558e46390896f78a6918852c86b1bf1181e37815
dyan Fedde <dyan@fedde.us>
2025-05-06 16:33:24 -0600

    Set up basic bones module

--- users/dyan.nix
+++ users/dyan.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
 
-  imports = [ ./fun.nix ];
+  imports = [
+    ./fun.nix
+    ./bones.nix
+  ];
   programs = {
     zsh.enable = true;
     _1password.enable = true;
@@ -18,27 +21,8 @@
     shell = pkgs.zsh;
   };
 
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
-  # Useful other development tools
-  environment.systemPackages = with pkgs; [
-    dive # look into docker image layers
-    podman-tui # status of containers in the terminal
-    docker-compose # start group of containers for dev
-    # podman-compose # start group of containers for dev
-  ];
-
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
   home-manager.users.dyan =
     { pkgs, ... }:
     {
@@ -51,6 +35,7 @@
         podman
         podman-compose
         spotify-player
+        obsidian
       ];
 
       programs = {
